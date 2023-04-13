1dbe4b3a8c5d33f00e73f5226ffab125040e6a50
engageintellect <jc9361@gmail.com>
2023-04-13 00:29:23 -0700

    adding api endpoint

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -11,20 +11,20 @@ export default function Counter() {
 
       <div class="flex flex-col sm:flex-row gap-2">
         <button
-          class="bg-white shadow-sm hover:shadow-md transition-all duration-300 py-2 px-4 rounded-md uppercase font-semibold text-xs"
+          class="bg-white w-full shadow-sm hover:shadow-md transition-all duration-300 py-2 px-4 rounded-md uppercase font-semibold text-xs"
           onClick={() => setCount(count() + 1)}
         >
-          Add
+          Increment
         </button>
         <button
-          class="bg-white shadow-sm hover:shadow-md transition-all duration-300 py-2 px-4 rounded-md uppercase font-semibold text-xs"
+          class="bg-white w-full shadow-sm hover:shadow-md transition-all duration-300 py-2 px-4 rounded-md uppercase font-semibold text-xs"
           onClick={() => setCount(count() - 1)}
         >
           Decrement
         </button>
 
         <button
-          class="bg-white shadow-sm hover:shadow-md transition-all duration-300 py-2 px-4 rounded-md uppercase font-semibold text-xs"
+          class="bg-white w-full shadow-sm hover:shadow-md transition-all duration-300 py-2 px-4 rounded-md uppercase font-semibold text-xs"
           onClick={() => setCount(0)}
         >
           Reset
